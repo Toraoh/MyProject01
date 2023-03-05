7e16a59f98d5cb3e866e0de2d6670d81d16f3c7f
Toraoh <126996004+Toraoh@users.noreply.github.com>
2023-03-05 13:09:12 +1100

    The MVC documents of a book store web application
    For a techicial test.
    Developed by using asp.net MVC framwork 4.6.1, VS2017 and SSMS 2017
    Need to install NuGet EntityFramework 6.2.0 and PageList,mvc 4.5.0

---
+++ Models/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+
+namespace MyProject01.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        public string BookId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Category { get; set; }
+
+        public string Author { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Language { get; set; }
+
+        public string OrderId { get; set; }
+    }
+}
